176a7db7daa10190723662c31e3e7dd37195d972
Camilia El Moustarih <e0958030@cmaisonneuve.qc.ca>
2024-03-24 17:46:20 -0400

    Ajout de liens externes pour le footer

--- footer.php
+++ footer.php
@@ -16,10 +16,12 @@
                         123 456 7890
                     </ul>
                     <ul>
+                        <a href="https://www.google.ca/maps">
                         <span class="material-symbols-outlined">pin_drop</span>
                         555, rue Omnivox
                         Montréal, Québec
                         (H2W 1G2)
+                        </a>
                     </ul>
                     <ul>
                         <span class="material-symbols-outlined">verified_user</span>
@@ -31,7 +33,7 @@
                     </ul>
                     <ul>
                         <span class="material-symbols-outlined">stacked_email</span>
-                        <a href="">Écrivez-nous</a>
+                        <a href="https://www.cmaisonneuve.qc.ca">Écrivez-nous</a>
                     </ul>
                 </div>
             </blockquote>
