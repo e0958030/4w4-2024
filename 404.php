251b64566e48eb1797ff272ab3f91341e909fd27
Camilia El Moustarih <119824587+e0958030@users.noreply.github.com>
2024-04-11 09:51:12 -0400

    s9c2 Examen Intra TP2 - Amélioration de la page 404, correction footer, ajout liens catégories et mise à jour index.htm

--- 404.php
+++ 404.php
@@ -5,10 +5,11 @@
 
 <!-- Contenu principal -->
 <div id="erreur_404" class="global">
-    <section class="entete__header">
-        <h2>Erreur 404</h2>   
+    <section>
+        <h2>Erreur 404</h2>
         <p>Vous essayez d'accéder à une page qui n'existe pas...</p>
-        <h4><?php echo get_bloginfo('name'); ?></h4> 
+        <p>Pour revenir à la page d'accueil, cliquez sur le lien suivant :</p>
+        <h4><?php echo get_bloginfo('name'); ?></h4>
         <h5><?php echo get_bloginfo('url'); ?></h5>
         <a href="<?php echo get_bloginfo('url'); ?>"> <?php echo get_bloginfo('name'); ?></a>
     </section>
@@ -16,21 +17,18 @@
 </div>
 
 <!-- Pied de page -->
-<div id="footer" class="global">
+<!-- <div id="footer" class="global">
     <div class="motif-footer">
         <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
             <path d="M602.45,3.86h0S572.9,116.24,281.94,120H923C632,116.24,602.45,3.86,602.45,3.86Z" style="fill:var(--couleur-arriere-galerie)"></path>
         </svg>
+    </div>   
+</div>  -->
+
+    <!-- Récupérer le footer php -->
+    <?php get_footer(); ?>
+    
+    <!-- Ajouter le menu avec toutes les catégories -->
+    <div id="liensCatFooter">
+        <?php wp_nav_menu(array("container" => "nav", "menu" => "principal")); ?>
     </div>
-    <footer>
-        <h2>Footer</h2>
-        <span>
-            <blockquote>Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque quae aut, voluptas dolor, veniam harum beatae, quos nostrum obcaecati quibusdam ad porro. Dolorum voluptatum consequatur blanditiis earum tempora provident repellat!</blockquote>
-
-            <!-- Bouton pour retour en haut de page -->
-            <a href="#menu"><button id="hautPage">Retour</button></a>
-    </footer>
-</div>
-</body>
-
-</html>
